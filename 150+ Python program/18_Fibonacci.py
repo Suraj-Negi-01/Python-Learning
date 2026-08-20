18e532f594d66891ae71473d28e827411c93f7f5
Suraj-Negi-01 <surajnegi3949@gmail.com>
2026-08-20 22:35:46 +0530

    added Fibonacci.py

---
+++ 150+ Python program/18_Fibonacci.py
@@ -0,0 +1,19 @@
+nterm = int (input("Enter the number of terms: "))
+
+n1, n2 = 0, 1
+count = 0
+
+if nterm <= 0:
+   print("Please enter a positive integer")
+
+elif nterm == 1:
+   print("Fibonacci sequence upto", nterm, ":")
+   print(n1)
+else:
+   print("Fibonacci sequence upto", nterm, ":")
+   while count < nterm:
+       print(n1, end=" ")
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
